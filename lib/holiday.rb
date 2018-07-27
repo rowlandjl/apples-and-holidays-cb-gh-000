1b44e4bd21e21334a30b5dc0b8e2e094f4afc886
Jessica Rowland <rowlandjl@vcu.edu>
2018-07-27 15:15:54 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,5 +68,5 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  holiday_hash.map { |season, holiday| holiday.map { |holiday_name, supply| holiday_name if supply.include?("BBQ") } }.flatten.compact
 end
